1c1bc018565241098e359475fbb77a5a61b0a844
BalalaievMaxim <balalaiev.m.yu.-im43@edu.kpi.ua>
2026-03-17 15:21:12 +0200

    feat: data & models

---
+++ lab1/src/mywebapp/Models/Note.cs
@@ -0,0 +1,9 @@
+namespace mywebapp.Models;
+
+public class Note
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
