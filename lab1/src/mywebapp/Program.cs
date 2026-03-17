618afeb2d7ca661aa3ab7af6a831d0ca9953e408
BalalaievMaxim <balalaiev.m.yu.-im43@edu.kpi.ua>
2026-03-17 15:30:51 +0200

    feat: endpoints & program

--- lab1/src/mywebapp/Program.cs
+++ lab1/src/mywebapp/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using mywebapp.Data;
+using mywebapp.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("/etc/mywebapp/config.json", optional: true, reloadOnChange: true);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+                       ?? "Host=127.0.0.1;Database=mywebappdb;Username=postgres;Password=postgres";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8000);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapSystemEndpoints();
+app.MapNotesEndpoints();
 
 app.Run();
